6175654b6d8a072c98ed5681ea359447565a7830
Rythem Datta <ritzy192@gmail.com>
2020-03-23 19:39:08 +0530

    ActionResult and convention based Routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,5 +15,19 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Hum Aapke Hai Kaun"};
             return View(movie);
         }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex = {0}&sotyBy = {1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("year = {0} \n month{1}", year, month));   
+        }
     }
 }
