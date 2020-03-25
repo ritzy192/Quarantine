7f7181eb85bb9ec520247aee1bb3c6b55be29750
Rythem Datta <ritzy192@gmail.com>
2020-03-25 15:54:25 +0530

    added entity framework, code first approach

---
+++ Udemy Vidly/Udemy Vidly/Migrations/202003251004337_PopulateMembershipTypes1.cs
@@ -0,0 +1,44 @@
+namespace Udemy_Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropTable("dbo.Customers");
+            DropTable("dbo.MembershipTypes");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        IsSubscribedToNewsletter = c.Boolean(nullable: false),
+                        MembershipTypeId = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "Id", cascadeDelete: true);
+        }
+    }
+}
--- Udemy Vidly/Udemy Vidly/Models/IdentityModels.cs
+++ Udemy Vidly/Udemy Vidly/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Vidly.Models;
 
 namespace Udemy_Vidly.Models
 {
@@ -20,6 +21,8 @@ namespace Udemy_Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        //public DbSet<Customer> Customers { get; set; }
+        //public DbSet<Movie> Movies { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
