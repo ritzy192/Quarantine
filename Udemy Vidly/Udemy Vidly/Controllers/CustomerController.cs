0b04e6e5571c1b4994ec141189c31533e4746de8
Rythem Datta <ritzy192@gmail.com>
2020-03-26 11:04:15 +0530

    Eager loading, fetching linked tables

--- Udemy Vidly/Udemy Vidly/Controllers/CustomerController.cs
+++ Udemy Vidly/Udemy Vidly/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Udemy_Vidly.Models;
@@ -20,7 +21,7 @@ namespace Vidly.Controllers
         }
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
