0681946ab9607c5c3e265aff1ffd6f92e9b504a8
Rythem Datta <ritzy192@gmail.com>
2020-03-26 10:55:01 +0530

    Fetching customer form db

--- Udemy Vidly/Udemy Vidly/Controllers/CustomerController.cs
+++ Udemy Vidly/Udemy Vidly/Controllers/CustomerController.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Vidly.Models;
+using Udemy_Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
             return View(customers);
         }
 
         public ActionResult Details(int Id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
             if (customer == null)
                 return HttpNotFound();
             else
                 return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Ankit" },
-                new Customer { Id = 2, Name = "Karan" }
-            };
         }
     }
 }
